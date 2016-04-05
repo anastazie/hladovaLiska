73eb35dbbb920a6787c9a2eed3501a763e9fbb0d
Anastassiya Zidkova <anastazie.d@gmail.com>
2016-04-05 23:22:09 +0200

    Hladova liska v0.1

---
+++ www/js/controllers.js
@@ -0,0 +1,42 @@
+angular.module('starter.controllers', [])
+
+.controller('stravenky', function($scope) {
+  $scope.isNumber = function (x) { 
+    console.log(x);
+    return Number.isNaN(x); 
+  };
+
+  function update() {
+    $scope.stravenek_ks = Math.floor($scope.total / $scope.stravenka);
+    $scope.cash = $scope.total - $scope.stravenka * $scope.stravenek_ks;
+  }
+  $scope.$watch("stravenka", update);
+  $scope.$watch("total", update);
+})
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
